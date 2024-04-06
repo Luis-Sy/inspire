8da59e20db2b882b84a9c9f0869a659d45586bad
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2024-04-06 00:43:18 -0600

    Added base for the ui using fragment layouts

---
+++ app/src/main/java/com/example/agendaassistant/TaskView.java
@@ -0,0 +1,22 @@
+package com.example.agendaassistant;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskView extends Fragment {
+
+    public TaskView(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_view, container, false);
+    }
+}
